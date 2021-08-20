301ab747aea0fd6f3a8fe334648675662a0b2443
Dishank422 <dishankjain0112358@gmail.com>
2021-08-20 15:50:45 +0530

    used built in norm function in assignment1/codes.computations.py

--- assignment1/codes/computations.py
+++ assignment1/codes/computations.py
@@ -4,8 +4,5 @@ A = numpy.array([1,-2,-8])
 B = numpy.array([5,0,-2])
 C = numpy.array([11,3,7])
 
-def norm(X):
-    return((X[0]**2+X[1]**2+X[2]**2)**(0.5))
-
-print("Lambda= ",norm(B-A)/norm(C-B),"=2/3")
+print("Lambda= ",numpy.linalg.norm(B-A)/numpy.linalg.norm(C-B),"=2/3")
 
