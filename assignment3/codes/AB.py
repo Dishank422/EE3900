d0b26f440ba067f5250b09e9fd71cb6f2994d7a3
Dishank422 <dishankjain0112358@gmail.com>
2021-08-26 07:21:31 +0530

    Assignment 3 completed

--- assignment3/codes/AB.py
+++ assignment3/codes/AB.py
@@ -1,4 +1,13 @@
-h = (5.5**2+5.5**2-5**2)/(2*5.5)
-k = (5.5**2-h**2)**(0.5)
-print("h=",h)
-print("k=",k)
+BC = 4.5
+CD = 5
+AD = 5.5
+AC = 5.5
+BD = 7
+
+def cordinates(PQ, PR, QR):
+    p1 = (PQ**2+QR**2-PR**2)/(2*QR)
+    return((p1,(PQ**2-p1**2)**(0.5)))
+
+print("B = ", cordinates(BC, BD, CD))
+print("A = ", cordinates(AC, AD, CD))
+
--- assignment3/codes/figure.py
+++ assignment3/codes/figure.py
@@ -13,10 +13,10 @@ from coeffs import *
 fig = plt.figure()
 
 #Points on the plane
-A = np.array([0,0]).reshape((2,1))
-B = np.array([1.68,-2.35]).reshape((2,1))
-C = np.array([5.5,0]).reshape((2,1))
-D = np.array([3.23,4.45]).reshape((2,1))
+A = np.array([2.5,4.9]).reshape((2,1))
+B = np.array([-0.375,4.48]).reshape((2,1))
+C = np.array([0,0]).reshape((2,1))
+D = np.array([5,0]).reshape((2,1))
 
 #Generating all lines
 x_AB = line_gen(A,B)
@@ -35,10 +35,10 @@ plt.scatter(A[0],A[1])
 plt.scatter(B[0],B[1])
 plt.scatter(C[0],C[1])
 plt.scatter(D[0],D[1])
-plt.text(0,0, "A", color='red')
-plt.text(1.68,-2.35, "B", color='red')
-plt.text(5.5,0, "C", color='red')
-plt.text(3.23,4.45, "D", color='red')
+plt.text(2.5,4.9, "A", color='red')
+plt.text(-0.375,4.48, "B", color='red')
+plt.text(0,0, "C", color='red')
+plt.text(5,0, "D", color='red')
 
 #save plot
 plt.xlabel('$x$');plt.ylabel('$y$')
