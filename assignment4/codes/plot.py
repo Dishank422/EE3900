c0a09ef7ec5f6f0494729800b48d7da77f2013e0
Dishank422 <dishankjain0112358@gmail.com>
2021-09-06 20:22:50 +0530

    assignment 4 and 5 updated

--- assignment4/codes/plot.py
+++ assignment4/codes/plot.py
@@ -14,15 +14,13 @@ V = np.array(([0,0],[0,1]))
 u = np.array(([-2,-1]))
 f = 5
 
-O = np.array(([0,0]))
-
 D_vec,P = LA.eig(V)
 D = np.diag(D_vec)
 
 p = P[:,0]
 eta = 2*u@p
 
-foc = eta/D_vec[1]
+foc = -eta/D_vec[1]
 
 x = parab_gen(y,foc)
 
@@ -30,9 +28,7 @@ cA = np.vstack((u+eta*0.5*p,V))
 cb = np.vstack((-f,(eta*0.5*p-u).reshape(-1,1)))
 c = LA.lstsq(cA,cb,rcond=None)[0]
 c = c.flatten()
-P=-P
 
-c1 = np.array(([(u@V@u-2*D_vec[0]*u@u+D_vec[0]**2*f)/(eta*D_vec[0]**2),0]))
 xStandardparab = np.vstack((x,y))
 
 xActualparab = P@xStandardparab + c[:,np.newaxis]
