d86feb559cb611487d1d7884481c61d0df52b547
Dishank422 <dishankjain0112358@gmail.com>
2021-10-02 12:43:09 +0530

    improvving accuracy

--- Gate-Assignment3/codes/DTFT.py
+++ Gate-Assignment3/codes/DTFT.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-omega = np.linspace(-2*np.pi-1,2*np.pi+1,100)
+omega = np.linspace(-2*np.pi-1,2*np.pi+1,1000)
 y = 1/(1-(5/6)*(np.exp(-1j*omega))) +  1/(1-(6/5)*(np.exp(-1j*omega)))
 
 amp = (y*np.conj(y))**(0.5)
