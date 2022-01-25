9c6388d735775643f03feb136522bc7548452e26
Dishank422 <dishankjain0112358@gmail.com>
2022-01-25 09:53:43 +0530

    synching global variables

--- filt/filt.py
+++ filt/filt.py
@@ -9,14 +9,14 @@ output_signal = np.zeros(len(input_signal))
 esp_serial = serial.Serial(port="/dev/ttyS0", baudrate=115200, timeout=10)
 esp_serial.flush()
 
-for i in range(len(input_signal)//450):
-    tmp = input_signal[450*i:450*(i+1)]
+for i in range(len(input_signal)//600):
+    tmp = input_signal[600*i:600*(i+1)]
     sent = {"d":tmp.tolist()}
     esp_serial.write(json.dumps(sent).encode('ascii'))
     rec = esp_serial.readline().decode('utf-8').rstrip()
     tmp2 = json.loads(rec)
     tmp3 = tmp2["d"]
-    output_signal[450*i:450*(i+1)] = np.array(tmp3)
+    output_signal[600*i:600*(i+1)] = np.array(tmp3)
     print(i)
 
 sf.write('output.wav',output_signal,fs)
